e006ac688d082f991e0925e33f48f2e2b94dd3c3
luisp <luis.p.vicente@gmail.com>
2024-08-21 11:51:41 +0200

    Commit

--- src/calibration_libraries.py
+++ src/calibration_libraries.py
@@ -29,7 +29,7 @@ def get_nearest_session(value: int, sessions: set) -> int:
 
 
 def select_master_dark(from_folder: str, params: dict) -> str:
-    src_darks = os.path.join(from_folder, r'master\darks')
+    src_darks = os.path.join(from_folder, r'masters\darks')
     darks_list = get_list_dir(src_darks, prefix='master', ext='fit')
     reduced_list = []
     for dark in darks_list:
@@ -70,20 +70,22 @@ def create_master_files(from_folder: str, image_type: str, siril_version: str = 
         if (os.path.exists(os.path.join(from_folder, out_root[image_type], 'master_' + folder + '.fit')) and
                 not force):
             continue
+
+        with open(r'..\templates\sequence_template.seq', 'r') as f_seq_template:
+            seq_template = f_seq_template.read()
+        with open(os.path.join(folder_path, folder + '_.seq'), 'w') as f_seq:
+            seq_content = seq_template.replace('{{sequence_name}}', folder)
+            seq_content = seq_content.replace('{{nb_images}}', str(nb_files))
+            seq_content = seq_content.replace('{{nb_selected}}', str(nb_files))
+            for f in file_list:
+                seq_number = int(f[:-4].split('_')[-1])
+                seq_content = seq_content + 'I ' + str(seq_number) + ' 1\n'
+            f_seq.write(seq_content)
+
         if nb_files > 1:
-            with open(r'..\templates\sequence_template.seq', 'r') as f_seq_template:
-                seq_template = f_seq_template.read()
-            with open(os.path.join(folder_path, folder + '.seq'), 'w') as f_seq:
-                seq_content = seq_template.replace('{{sequence_name}}', folder)
-                seq_content = seq_content.replace('{{nb_images}}', str(nb_files))
-                seq_content = seq_content.replace('{{nb_selected}}', str(nb_files))
-                for f in file_list:
-                    seq_number = int(f[:-4].split('_')[-1])
-                    seq_content = seq_content + 'I ' + str(seq_number) + ' 1\n'
-                f_seq.write(seq_content)
             with open(r'..\templates\create_master_' + image_type + '_template.ssf', 'r') as f_script_template:
                 master_template = f_script_template.read()
-            with open(os.path.join(folder_path, 'create_master_' + image_type + '_.ssf'), 'w') as f_ssf:
+            with open(os.path.join(folder_path, 'create_master_' + image_type + '.ssf'), 'w') as f_ssf:
                 out_file = os.path.join(from_folder, out_root[image_type], 'master_' + folder + '.fit')
                 rel_out_path = os.path.relpath(out_file, os.path.join(src_root, folder))
                 script_content = master_template.replace('{{siril_version}}', siril_version)
@@ -104,17 +106,24 @@ def create_master_files(from_folder: str, image_type: str, siril_version: str = 
             if image_type == 'darks':
                 in_file = os.path.join(folder_path, file_list[0])
                 out_file = os.path.join(from_folder, out_root[image_type], 'master_' + folder + '.fit')
+                with open(r'..\templates\sequence_stats_template.ssf', 'r') as f_script_template:
+                    master_template = f_script_template.read()
+                with open(os.path.join(folder_path, 'sequence_stats.ssf'), 'w') as f_ssf:
+                    script_content = master_template.replace('{{siril_version}}', siril_version)
+                    script_content = script_content.replace('{{sequence}}', folder)
+                    f_ssf.write(script_content)
+                subprocess.run('siril-cli -d ' + folder_path + ' -s ' +
+                               os.path.join(folder_path, 'sequence_stats.ssf'))
                 copy_file(in_file, out_file)
             elif image_type == 'flats':
-                in_file = os.path.join(folder_path, file_list[0])
+                in_file = os.path.join(folder_path, 'pp_' + file_list[0])
                 out_file = os.path.join(from_folder, out_root[image_type], 'master_' + folder + '.fit')
                 with open(r'..\templates\calibrate_single_file_template.ssf', 'r') as f_script_template:
                     master_template = f_script_template.read()
                 with open(os.path.join(folder_path, 'create_master_' + image_type + '_.ssf'), 'w') as f_ssf:
-                    out_file = os.path.join(from_folder, out_root[image_type], 'master_' + folder + '.fit')
-                    rel_out_path = os.path.relpath(out_file, os.path.join(src_root, folder))
                     script_content = master_template.replace('{{siril_version}}', siril_version)
                     script_content = script_content.replace('{{image}}', file_list[0])
+                    script_content = script_content.replace('{{sequence}}', folder)
                     try:
                         master_dark = select_master_dark(from_folder, get_fields_from_foldername(folder))
                     except NoSuitableDarkAvailable:
@@ -124,14 +133,23 @@ def create_master_files(from_folder: str, image_type: str, siril_version: str = 
                     f_ssf.write(script_content)
                 subprocess.run('siril-cli -d ' + folder_path + ' -s ' +
                                os.path.join(folder_path, 'create_master_' + image_type + '.ssf'))
+
+                copy_file(in_file, out_file)
             else:
                 continue
+
+        # TODO: standardize stats files
+
         if clean:
             clean_dir(folder_path, prefix='pp_', ext='.fit')
+
+    # TODO: obtain stats for master files
+
+    # TODO: compile all stats
 
 
 if __name__ == '__main__':
     start_folder = os.path.normpath(r'D:\AstroProjects')
 
-    # create_master_files(start_folder, 'dark')
+    create_master_files(start_folder, 'dark')
     create_master_files(start_folder, 'flat')

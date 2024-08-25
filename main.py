46e054214d1321d38498d2ee6c0c7cc079a3cf72
luisp <luis.p.vicente@gmail.com>
2024-08-25 21:02:40 +0200

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from src.asiair_import import initialize_folders, read_asiair_files, update_metadata, import_files
-from src.calibration_files import create_sequence
+from src.calibration_files import create_all_sequences
 
 astroprojects_folder = Path(r'D:\AstroProjects')
 asiar_root_folder = Path(r'D:\Asiair')
@@ -12,6 +12,4 @@ sources_paths = {'dark': astroprojects_folder / r'sources/darks', 'flat': astrop
 # update_metadata(astroprojects_folder)
 # import_files(asiar_root_folder, astroprojects_folder)
 
-# create_sequence(Path(r'D:\AstroProjects\sources\darks\Dark_20230729_04_180000.0ms_Bin1_294MC_Gain120_-10.0C'))
-dir_list = [x for x in list(sources_paths['dark'].glob('*')) if x.is_dir()]
-print(dir_list[0].stem)
+create_all_sequences(astroprojects_folder / 'sources')

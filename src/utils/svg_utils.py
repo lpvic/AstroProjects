5b9b9001c27ba1a989dd40e1f39f48f2a5dec6ab
luispedro <luis.p.vicente@gmail.com>
2024-09-06 07:46:58 +0200

    Commit

--- src/utils/svg_utils.py
+++ src/utils/svg_utils.py
@@ -1,8 +1,10 @@
 import base64
 
+with open(r'../../templates/landscape_template.svg', 'r') as svg_template:
+    template_content = svg_template.read()
+
+with open(r'test.png', 'rb') as png_file:
+    encoded_string = base64.b64encode(png_file.read()).decode('utf-8')
+
 with open('svg_test.svg', 'w') as svg_file:
-    svg_file.write(r'<svg width="4144" height="2822" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' + '\n')
-    with open(r'C:\Users\luisp\Pictures\Siril\2024.07.27_M31_02\results\2024.07.25_M31_02_LPro_v1.0.png', 'rb') as png_file:
-        encoded_string = base64.b64encode(png_file.read())
-    svg_file.write(r'<image width="100%" height="100%" xlink:href="data:image/png;base64,' + encoded_string.decode('utf-8') + '" />' + '\n')
-    svg_file.write(r'</svg>')
+    svg_file.write(template_content.replace('{{image_data}}', encoded_string))

ef77ea28b9b6270bf448a3b39990596fde9b1424
luisp <luis.p.vicente@gmail.com>
2024-09-16 22:17:00 +0200

    Commit

--- src/utils/svg_utils.py
+++ src/utils/svg_utils.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 import base64
 
+from PIL import Image
+from matplotlib import pyplot as plt
+from astropy.io import fits
+import numpy as np
+import subprocess
+
 
 def sign_svg(svg_image: Path, png_image: Path, title: str = '', signture: str = '') -> None:
     with open(r'../../templates/landscape_template.svg', 'r') as svg_template:
@@ -18,3 +24,25 @@ def sign_svg(svg_image: Path, png_image: Path, title: str = '', signture: str = 
 if __name__ == '__main__':
     sign_svg(Path(r'svg_test.svg'), Path(r'test.png'), 'Galaxia de Andrómeda (M31)',
              r'13/09/2024, Luis Pedro Vicente Matilla')
+
+    arr = fits.getdata('test.fit')
+    r = arr[0, :, :]
+    g = arr[1, :, :]
+    b = arr[2, :, :]
+    rgb = np.zeros((2822, 4144, 3))
+    rgb[:, :, 0] = r
+    rgb[:, :, 1] = g
+    rgb[:, :, 2] = b
+    print(rgb.shape)
+
+    dpi = 1000.
+    fig = plt.figure(frameon=False)
+    fig.set_size_inches(w=4144/dpi, h=2822/dpi)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    plt.imshow(rgb, origin='lower')
+    plt.show()
+    fig.savefig('iiii.png', dpi=dpi)
+
+    subprocess.run('inkscape --export-type="png" svg_test.svg')
